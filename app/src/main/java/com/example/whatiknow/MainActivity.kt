5ce794409b4e6a5f90aa7acbb36ef51ca7f664ef
Mikhail Kashin <144782206+Mikkkkkkka@users.noreply.github.com>
2026-02-22 01:26:29 +0300

    small experiments

--- app/src/main/java/com/example/whatiknow/MainActivity.kt
+++ app/src/main/java/com/example/whatiknow/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.whatiknow
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +17,13 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val button = findViewById<Button>(R.id.button)
+        val helloText = findViewById<TextView>(R.id.helloText)
+
+        button.setOnClickListener {
+            helloText.text = getString(R.string.button_pressed)
         }
     }
 }
