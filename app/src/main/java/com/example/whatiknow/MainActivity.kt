e707fee1d56093964fd5a20f3f6ac97c34204f8c
Mikhail Kashin <144782206+Mikkkkkkka@users.noreply.github.com>
2026-02-23 00:20:58 +0300

    added a date textView to the MainActivity.kt with placeholder onClick functionality

--- app/src/main/java/com/example/whatiknow/MainActivity.kt
+++ app/src/main/java/com/example/whatiknow/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.example.whatiknow
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 
 class MainActivity : AppCompatActivity() {
+
+    var date: TextView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         WindowCompat.setDecorFitsSystemWindows(window, false)
@@ -18,5 +24,18 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, bottomInset)
             insets
         }
+
+        date = findViewById(R.id.dateTextView)
+        date?.setOnClickListener { incrementDate() }
+    }
+
+    private fun openCalendar() {}
+
+    private fun incrementDate() {
+        val text = date?.text?.toString() ?: return
+        val formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy")
+        val currentDate = LocalDate.parse(text, formatter)
+        val newDate = currentDate.plusDays(1)
+        date?.text = newDate.format(formatter)
     }
 }
