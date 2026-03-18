cdd56828f2cc0bf55325f8e473f8a892447c2898
Mikhail Kashin <144782206+Mikkkkkkka@users.noreply.github.com>
2026-03-18 20:20:45 +0300

    added LocalMarkRepository.kt

---
+++ app/src/main/java/dev/mikkkkkkka/whatiknow/data/mapper/RoomMarkEntityMapper.kt
@@ -0,0 +1,22 @@
+package dev.mikkkkkkka.whatiknow.data.mapper
+
+import dev.mikkkkkkka.whatiknow.data.local.entities.MarkEntity
+import dev.mikkkkkkka.whatiknow.domain.model.Mark
+import java.time.LocalDateTime
+
+class RoomMarkEntityMapper {
+    fun map(roomMark: MarkEntity): Mark {
+        return Mark(
+            roomMark.date,
+            roomMark.content,
+        )
+    }
+
+    fun unmap(note: Mark, updatedAt: LocalDateTime): MarkEntity {
+        return MarkEntity(
+            note.date,
+            note.content,
+            updatedAt,
+        )
+    }
+}
