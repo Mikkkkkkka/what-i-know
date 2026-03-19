6c26d199f2514e5454e01c6159864831bad09e34
Mikhail Kashin <144782206+Mikkkkkkka@users.noreply.github.com>
2026-03-19 17:57:31 +0300

    added WorkspaceActivity.kt and NotesAdapter.kt

---
+++ app/src/main/java/dev/mikkkkkkka/whatiknow/ui/workspace/NotesAdapter.kt
@@ -0,0 +1,44 @@
+package dev.mikkkkkkka.whatiknow.ui.workspace
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import dev.mikkkkkkka.whatiknow.R
+
+class NotesAdapter(
+    private val onClick: (noteId: String) -> Unit
+) : RecyclerView.Adapter<NotesAdapter.ViewHolder>() {
+
+    private val noteIds = mutableListOf<String>()
+
+    fun submitList(list: List<String>) {
+        noteIds.clear()
+        noteIds.addAll(list)
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater
+            .from(parent.context)
+            .inflate(R.layout.item_note, parent, false)
+
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(noteIds[position], onClick)
+    }
+
+    override fun getItemCount(): Int = noteIds.size
+
+    class ViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
+        private val title: TextView = view.findViewById(R.id.noteTitle)
+
+        fun bind(noteId: String, onClick: (String) -> Unit) {
+            title.text = noteId
+            view.setOnClickListener { onClick(noteId) }
+        }
+    }
+}
---
+++ app/src/main/java/dev/mikkkkkkka/whatiknow/ui/workspace/WorkspaceViewModel.kt
@@ -0,0 +1,22 @@
+package dev.mikkkkkkka.whatiknow.ui.workspace
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dev.mikkkkkkka.whatiknow.domain.usecase.note.GetNoteIdsUseCase
+import kotlinx.coroutines.launch
+
+class WorkspaceViewModel(
+    private val getNoteIds: GetNoteIdsUseCase
+) : ViewModel() {
+
+    private val innerNotes = MutableLiveData<List<String>>()
+    val notes: LiveData<List<String>> = innerNotes
+
+    fun loadNotes() {
+        viewModelScope.launch {
+            innerNotes.value = getNoteIds()
+        }
+    }
+}
