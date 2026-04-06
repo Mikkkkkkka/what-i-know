82bf74c7e546e825c5bd074cadcec1a94b45a242
Mikhail Kashin <144782206+Mikkkkkkka@users.noreply.github.com>
2026-04-07 02:01:58 +0300

    di: re-routed di to use the new backend-aware repositories

--- app/src/main/java/dev/mikkkkkkka/whatiknow/di/RepositoryModule.kt
+++ app/src/main/java/dev/mikkkkkkka/whatiknow/di/RepositoryModule.kt
@@ -4,8 +4,10 @@ import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
-import dev.mikkkkkkka.whatiknow.data.repository.LocalMarkRepository
-import dev.mikkkkkkka.whatiknow.data.repository.LocalNoteRepository
+import dev.mikkkkkkka.whatiknow.data.repository.AuthRepositoryImpl
+import dev.mikkkkkkka.whatiknow.data.repository.SyncedMarkRepository
+import dev.mikkkkkkka.whatiknow.data.repository.SyncedNoteRepository
+import dev.mikkkkkkka.whatiknow.domain.repository.AuthRepository
 import dev.mikkkkkkka.whatiknow.domain.repository.MarkRepository
 import dev.mikkkkkkka.whatiknow.domain.repository.NoteRepository
 import javax.inject.Singleton
@@ -16,14 +18,20 @@ abstract class RepositoryModule {
 
     @Binds
     @Singleton
+    abstract fun bindAuthRepository(
+        repository: AuthRepositoryImpl,
+    ): AuthRepository
+
+    @Binds
+    @Singleton
     abstract fun bindNoteRepository(
-        repository: LocalNoteRepository,
+        repository: SyncedNoteRepository,
     ): NoteRepository
 
     @Binds
     @Singleton
     abstract fun bindMarkRepository(
-        repository: LocalMarkRepository,
+        repository: SyncedMarkRepository,
     ): MarkRepository
 }
 
