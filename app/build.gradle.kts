822618e70c76601b9dcd3f91047610c4c7c0b737
Mikhail Kashin <144782206+Mikkkkkkka@users.noreply.github.com>
2026-04-06 22:13:38 +0300

    add buildConfig and related fields and buildTypes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,12 +22,19 @@ android {
     }
 
     buildTypes {
+        debug {
+            buildConfigField("String", "API_BASE_URL", "\"http://10.0.2.2:8080\"")
+        }
         release {
             isMinifyEnabled = false
             proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
+                getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro"
             )
+            buildConfigField("String", "API_BASE_URL", "\"https://api.what-i-know.mikkkkkkka.ru\"")
+        }
+        create("staging") {
+            initWith(getByName("release"))
+            signingConfig = signingConfigs.getByName("debug")
         }
     }
     compileOptions {
@@ -38,6 +45,7 @@ android {
 
     buildFeatures {
         viewBinding = true
+        buildConfig = true
     }
 }
 
